01b5b6c7a36713dd6d76bd53fbc5102a86ca92cf
Josue Morales <157775085+PR2014FL@users.noreply.github.com>
2024-05-13 11:25:55 -0400

    Added Counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,58 @@
-import { useState } from 'react'
+import React, { useReducer } from 'react'//1. add useReducer
+import './App.css'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css'
+//2. Make my reducer that aligns with initial state
+const reducer = (state, action) => {
+  //This is where the switch statement goes. Think of think as the 'playbook'or 'SOP'
+ switch (action.type) {
+  case "INCREMENT":
+    return{
+      count: state.count +1,
+      toggleText: state.toggleText
+    }
+    break;
+  case "DECREMENT":
+    return{
+      count: state.count -1,
+      toggleText: state.toggleText
+    }
+  break;
+  case "toggleText":
+    return{
+      count: state.count,
+      toggleText: !state.toggleText
+    }
+  default:
+    return state
+ }
+}
 
 function App() {
   // const [count, setCount] = useState(0)
 
+
+
+const[count, dispatch] = useReducer(reducer, {
+  count: 0,
+  toggleText: true
+})
+// connects the reducer function to the dispatch action
+
   return (
     <>
-    <div> Hi there</div>
+    <div> 
+
+      
+<h1>Counter Application</h1>
+<div className="btnContainer">
+  <button onClick={() => {dispatch({type: "DECREMENT"})}}> - </button>
+  <button onClick={() => {dispatch({type: "INCREMENT"})}}> + </button>
+</div>
+<h2>Current Count: {count.count}</h2>
+
+
+    </div>
       {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
